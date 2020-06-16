639552bbd61a2114b4ce053e79d66dcb9dc7e65f
Simon <mySimonID@gmail.com>
2020-06-16 15:17:00 +0100

    Updates

---
+++ precache-manifest.70c5f16c8bef2bd84f179aadfa6fd0c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9da8091d46cf66dd1a820ae872267ab",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "1b0e623e6807803020a1",
+    "url": "/Calculator/static/css/main.30f0bd61.chunk.css"
+  },
+  {
+    "revision": "2ffd035b541b259e51bd",
+    "url": "/Calculator/static/js/2.8d0a5fdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.8d0a5fdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b0e623e6807803020a1",
+    "url": "/Calculator/static/js/main.5bfe2825.chunk.js"
+  },
+  {
+    "revision": "cf20fc91f11753727d2e",
+    "url": "/Calculator/static/js/runtime-main.891a279e.js"
+  }
+]);
